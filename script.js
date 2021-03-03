c85c9093881769704e0332986bd9a84bf8486b50
Vitaliy V <vitalik.voronov1989@ya.ru>
2021-03-03 03:25:09 +0300

    the end

---
+++ script.js
@@ -0,0 +1,77 @@
+
+$(".slider").slider({
+  animate: "0.3",
+  max: 255,
+  min: 0,
+  value: 127,
+  range: "min",
+  slide: handleSliderColor(setBGColor)
+});
+
+$(".switch-color-control").button()
+
+
+/**
+ * Принимает показания слайдеров, и собирает их в строку вида 'rgb(xxx, xxx, xxx)', и передает в коллбек "cb"
+ * ..наверно следовало разделить на две функции
+ * @param {function} cb 
+ */
+function handleSliderColor(cb) {
+  const rgbChannels = [127, 127, 127];
+  return function(ev, ui) {
+    $(this).is($("#red"))
+      ? rgbChannels[0] = ui.value
+    : $(this).is($("#green"))
+      ? rgbChannels[1] = ui.value
+    : $(this).is($("#blue"))
+      ? rgbChannels[2] = ui.value
+      : console.warn("Just do refactoring");
+    
+    cb(`rgb(${rgbChannels.toString()})`)
+  }
+}
+
+/**
+ * Из строки вида 'rgb(xxx, xxx, xxx)' забирает числа в массив, альфа-канал игнорирует
+ * @param {string} rgbStr - RGB(RGBA) like string
+ */
+function rgbStringToArray(rgbStr) {
+  return rgbStr.match(/rgba?\((\d+), ?(\d+), ?(\d+).*\)/).slice(1);
+}
+
+/**
+ * Крутит слайдеры в цвет
+ * @param {number[]} rgbColorArr - values of rgb channels
+ */
+function setSliderColor(rgbColorArr) {
+  $("#red.slider").slider("value", rgbColorArr[0])
+  $("#green.slider").slider("value", rgbColorArr[1])
+  $("#blue.slider").slider("value", rgbColorArr[2])
+}
+
+// Переключает слайдеры, и крутит их как надо ;) 
+$(".switch-color-control").change(ev => {
+  if (ev.target.value == "bg-color") {
+
+    const rgbArr = rgbStringToArray($("#preview-bg").css("background-color"));
+    setSliderColor(rgbArr);
+    $('.slider').slider("option", { slide: handleSliderColor(setBGColor) });
+
+  } else if (ev.target.value == "text-color") {
+
+    const rgbArr = rgbStringToArray($("#preview-text").css("color"));
+    setSliderColor(rgbArr);
+    $('.slider').slider("option", { slide: handleSliderColor(setTextColor) });
+
+  } else {
+    console.warn("Just do refactoring");
+  }
+})
+
+function setBGColor(color) {
+  $("#preview-bg").css("background-color", color)
+}
+
+function setTextColor(color) {
+  $("#preview-text").css("color", color)
+}
